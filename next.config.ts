eeb920a71f9986a1f73150e3383c96b6a2acf022
Stephane Moroso <stephane.moroso@gmail.com>
2025-03-09 12:29:50 +0100

    fix: Configure Base Path to Fix Missing Images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
-  const nextConfig: NextConfig = {
-    output: "export",
-    reactStrictMode: true,
-  };
+const nextConfig: NextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: `${process.env.PAGES_BASE_PATH}/`,
+};
 
-  export default nextConfig;
+export default nextConfig;
